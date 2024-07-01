a84868190fe1dd9d7957a80b7041f6470f0f109d
Roberto Russo <roberto.russo@aulab.it>
2024-07-01 10:22:30 +0200

    cambiata leggermente la home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,9 @@
 <x-layout>
-    <h1 class="text-white diplay-1">Home page messa online hack146</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-dark diplay-1">Home page messa online hack146</h1>
+            </div>
+        </div>
+    </div>
 </x-layout>

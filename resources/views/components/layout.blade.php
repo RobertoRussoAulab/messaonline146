e196d8360a6c4cfc203f24a863398dfb250885cc
Roberto Russo <roberto.russo@aulab.it>
2024-07-01 10:16:49 +0200

    fatto building degli asset

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DemoMessaOnline246</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-custom-robbiecss">
+    {{$slot}}
+</body>
+</html>
